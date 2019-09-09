583f3f0491b536b9b5562d3f4fb24e8e2caad575
macorios22 <mark0android90@gmail.com>
2019-09-09 12:07:04 -0600

    Vistas de edicion y nuevo agregadas con exito

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('',views.listar_pub, name='listar_pub'),
-}
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
 from .models import Publicacion
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.shortcuts import redirect
+
 
 # Create your views here.
 def listar_pub(request):
     pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_pub.html', {'pubs': pubs})
+
+def post_detail(request, pk):
+    pubss = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/post_detail.html', {'pubss': pubss})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_publicacion = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Publicacion, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.autor = request.user
+            post.fecha_publicacion = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
